b0f59ef1a25027a24e4d1a266d79f15813f072cb
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2019-12-13 11:27:58 +0100

    feat(Login): init form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Header from "./layout/Header/Header";
 import "./App.css";
 import Login from "./Login/Login";
 import Dashboard from "./Dashboard/Dashboard";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App = () => (
   <div className="App">
     <Header />
-    <Login />
-    <Dashboard />
+    <Router>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="login">Login</Link>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
+    </Router>
   </div>
 );
 
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,33 @@
 import React from "react";
 import "./Login.css";
 
-const Login = () => <div class="login">Login</div>;
+const Login = () => (
+  <div class="login">
+    <form class="login">
+      <label for="email">Login</label>
+      <input id="email" type="email" class="login__email" placeholder="Email" />
+      <label for="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        class="login__password"
+        placeholder="Password"
+      />
+      <h3>Password strength</h3>
+      <progress
+        class="password-strength__progress"
+        max="5"
+        value="0"
+      ></progress>
+      <div class="password-strength__bar-container">
+        <div class="password-strength__bar">
+          <span class="password-strength__value"></span>
+        </div>
+      </div>
+      <h3 class="login__error login__error--hide">Bad credentials</h3>
+      <button class="login__button">Login</button>
+    </form>
+  </div>
+);
 
 export default Login;
