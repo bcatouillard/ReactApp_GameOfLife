e05ee00ecd7099dad446d6c2dc093c7e2b1b3325
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2019-12-13 10:49:52 +0100

    feat: init boilerplate

---
+++ src/layout/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+  <header class="header">
+    <h1 class="header_title">Game of Life</h1>
+    <h2 class="header_subtitle">App</h2>
+  </header>
+);
+
+export default Header;
